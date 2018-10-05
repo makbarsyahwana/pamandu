0f61e653de4208ea9f82ac7976444ef412acb2e6
makbarsyahwana <makbarsyahwana@gmail.com>
2018-10-05 07:50:30 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,7 @@ router.get('/cart', function(req, res, next) {
 		return res.render('shop/shopping-cart', { products: null });
 	}
 	var cart = new Cart(req.session.cart);
-	res.render('layouts/shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+	res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 
